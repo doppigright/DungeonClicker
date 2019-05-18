b6ba948cd2636988fd0f40b98ba96c9275e92aef
Павел Кравченко <pavellevap98@mail.ru>
2019-05-18 17:19:32 +0700

    First commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}

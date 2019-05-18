5470b5ac869abc8eacfb6be5e6362b62f9965eb5
Павел Кравченко <pavellevap98@mail.ru>
2019-05-19 02:23:27 +0700

    add level-transitions

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -13,4 +13,27 @@ public class Button : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().color = Color.white;
     }
+
+    [System.Obsolete]
+    private void OnMouseUpAsButton()
+    {
+        switch (gameObject.name)
+        {
+            case "play":
+                Application.LoadLevel("Dungeon&Home");
+                break;
+            case "cog":
+                Application.LoadLevel("Settings");
+                break;
+            case "sideswipe":
+                Application.LoadLevel("Menu");
+                break;
+            case "hut":
+                Application.LoadLevel("Home");
+                break;
+            case "dungeon-gate":
+                Application.LoadLevel("Dungeon");
+                break;
+        }
+    }
 }

4831f83807facd0b31d0611d6199a0951c873595
Павел Кравченко <pavellevap98@mail.ru>
2019-05-21 00:18:51 +0700

    add Click damage

---
+++ Assets/Scripts/Dungeon/Hit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public Monster monster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log($"tab");
+        monster.GetHit(10);
+    }
+}
